215f9a27ec040083f0341966db874b520cf80fc3
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-01 17:52:40 +0400

    add xpo

---
+++ CS/XPO/NonPersistentObjectsDemo/NonPersistentObjectsDemo.Module/BusinessObjects/Account.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+
+namespace NonPersistentObjectsDemo.Module.BusinessObjects {
+    [DefaultClassOptions]
+    [DefaultListViewOptions(true, NewItemRowPosition.Top)]
+    [DefaultProperty("PublicName")]
+   
+    [DevExpress.ExpressApp.DC.DomainComponent]
+    public class Account : NonPersistentObjectBase {
+        private string _myKey;
+        [DevExpress.ExpressApp.ConditionalAppearance.Appearance("", Enabled = false, Criteria = "Not IsNewObject(This)")]
+        [RuleRequiredField]
+        [DevExpress.ExpressApp.Data.Key]
+        public string MyKey {
+            get { return _myKey; }
+            set { _myKey = value; }
+        }
+   
+        private string publicName;
+        public string PublicName {
+            get { return publicName; }
+            set { SetPropertyValue(nameof(PublicName), ref publicName, value); }
+        }
+    }
+}
---
+++ CS/XPO/NonPersistentObjectsDemo/NonPersistentObjectsDemo.Module/ServiceClasses/NonPersistentObjectFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
+
+namespace NonPersistentObjectsDemo.Module {
+
+    public abstract class NonPersistentObjectFactoryBase {
+        public abstract object GetObjectByKey(Type objectType, object key);
+        public abstract IEnumerable GetObjects(Type objectType, CriteriaOperator criteria, IList<DevExpress.Xpo.SortProperty> sorting);
+        public virtual void SaveObjects(ICollection toInsert, ICollection toUpdate, ICollection toDelete) { }
+    }
+}
